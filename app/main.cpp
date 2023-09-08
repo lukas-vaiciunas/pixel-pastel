e11e2eff41117568f62064aef67d80a6da27a8ce
Lukas Vaiciunas <lvjuodasuo@gmail.com>
2023-09-08 15:32:29 -0400

    Set up SFML window and main loop

--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,33 @@
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+
 int main(void)
 {
+	const sf::Color clearColor = sf::Color(255, 0, 255);
+
+	sf::RenderWindow window(
+		sf::VideoMode(1600, 900, 32),
+		"Pixel Pastel",
+		sf::Style::Titlebar | sf::Style::Close);
+
+	window.setFramerateLimit(60);
+	window.setKeyRepeatEnabled(false);
+
+	while (window.isOpen())
+	{
+		sf::Event ev;
+
+		while (window.pollEvent(ev))
+		{
+			if (ev.type == sf::Event::EventType::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear(clearColor);
+		window.display();
+	}
+
 	return 0;
 }
