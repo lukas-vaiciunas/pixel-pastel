e2e25582a0c8a1ce72edc866acfabe5d88ee9c34
Lukas Vaiciunas <lvjuodasuo@gmail.com>
2023-09-09 00:09:43 -0400

    Add `Config`

---
+++ app/Config.cpp
@@ -0,0 +1,16 @@
+#include "Config.h"
+
+namespace Config
+{
+	namespace Window
+	{
+		unsigned int width = 1600;
+		unsigned int height = 900;
+		unsigned int bitsPerPixel = 32;
+	}
+
+	namespace Interface
+	{
+		float scale = 2.0f;
+	}
+}
---
+++ app/Config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Config
+{
+	namespace Window
+	{
+		extern unsigned int width;
+		extern unsigned int height;
+		extern unsigned int bitsPerPixel;
+	}
+
+	namespace Interface
+	{
+		extern float scale;
+	}
+}
